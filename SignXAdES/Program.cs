cf7ce35436b3ed0e167d9175e8dd46e6b18906eb
RGNGIT <vnifedow@bk.ru>
2024-06-21 14:42:59 +0500

    Перевод на шарпей

--- SignXAdES/Program.cs
+++ SignXAdES/Program.cs
@@ -1,64 +1,95 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
 using System.Security.Cryptography.Xml;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Xml;
-using GostCryptography.Xml;
+using CryptoPro.Sharpei.Xml;
 
-class Program
+namespace Directum
 {
-    public static void SignXml(string xmlFilePath, string certificateThumbprint)
+    class SignDocument
     {
-        XmlDocument xmlDoc = new XmlDocument();
-        xmlDoc.PreserveWhitespace = true;
-        xmlDoc.Load(xmlFilePath);
-
-        X509Certificate2 certificate = GetCertificateByThumbprint(certificateThumbprint);
-
-        var signedXml = new GostSignedXml(xmlDoc);
-        signedXml.SigningKey = certificate.GetRSAPrivateKey();
-
-        Reference reference = new Reference();
-        reference.Uri = "";
-        reference.AddTransform(new XmlDsigEnvelopedSignatureTransform());
-        reference.AddTransform(new XmlDsigC14NTransform());
-        reference.DigestMethod = "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34112012-256";
-        signedXml.AddReference(reference);
-
-        KeyInfo keyInfo = new KeyInfo();
-        keyInfo.AddClause(new KeyInfoX509Data(certificate));
-        signedXml.KeyInfo = keyInfo;
-        signedXml.ComputeSignature();
-
-        XmlElement xmlDigitalSignature = signedXml.GetXml();
-        xmlDoc.DocumentElement.AppendChild(xmlDoc.ImportNode(xmlDigitalSignature, true));
-        xmlDoc.Save("./Certs/Signed_XML.xml");
-
-        Console.WriteLine("Документ подписан");
-    }
-
-    public static X509Certificate2 GetCertificateByThumbprint(string thumbprint)
-    {
-        using (X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+        [STAThread]
+        static void Main(string[] args)
         {
-            store.Open(OpenFlags.ReadOnly);
-            foreach (X509Certificate2 cert in store.Certificates)
+            try
             {
-                if (cert.Thumbprint.Equals(thumbprint, StringComparison.OrdinalIgnoreCase))
+                X509Certificate2Collection found;
+
+                X509Store store = new X509Store(StoreLocation.LocalMachine);
+                store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadOnly);
+                found = store.Certificates.Find(X509FindType.FindByThumbprint, System.IO.File.ReadAllText("./Certs/thumbprint.txt").Trim(' '), false);
+
+                if (found.Count == 0)
                 {
-                    return cert;
+                    store = new X509Store(StoreLocation.CurrentUser);
+                    store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadOnly);
+                    found = store.Certificates.Find(X509FindType.FindByThumbprint, System.IO.File.ReadAllText("./Certs/thumbprint.txt").Trim(' '), false);
                 }
+
+                if (found.Count == 0)
+                    throw new Exception("Сертификат не найден.");
+
+                X509Certificate2 Certificate = found[0];
+
+                AsymmetricAlgorithm Key = Certificate.PrivateKey;
+                SignXmlFile("./Certs/tosign.xml", "./Certs/signed.xml", Key, Certificate);
+                Console.WriteLine("XML подписан.");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        // Подписываем XML файл и сохраняем его в новом файле.
+        static void SignXmlFile(string FileName,
+            string SignedFileName, AsymmetricAlgorithm Key,
+            X509Certificate Certificate)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            doc.Load(new XmlTextReader(FileName));
+
+            SignedXml signedXml = new SignedXml(doc);
+            signedXml.SigningKey = Key;
+
+            Reference reference = new Reference();
+            reference.Uri = "";
+            reference.DigestMethod = CPSignedXml.XmlDsigGost3411Url;
+
+
+            XmlDsigEnvelopedSignatureTransform env = new XmlDsigEnvelopedSignatureTransform();
+            reference.AddTransform(env);
+
+            XmlDsigSmevTransform smev = new XmlDsigSmevTransform(); 
+            reference.AddTransform(smev);
+
+            XmlDsigC14NTransform c14 = new XmlDsigC14NTransform();
+            reference.AddTransform(c14);
+            signedXml.AddReference(reference);
+
+            KeyInfo keyInfo = new KeyInfo();
+            keyInfo.AddClause(new KeyInfoX509Data(Certificate));
+            signedXml.KeyInfo = keyInfo;
+            signedXml.ComputeSignature();
+
+            XmlElement xmlDigitalSignature = signedXml.GetXml();
+            doc.DocumentElement.AppendChild(doc.ImportNode(xmlDigitalSignature, true));
+
+            if (doc.FirstChild is XmlDeclaration)
+            {
+                doc.RemoveChild(doc.FirstChild);
+            }
+
+            using (XmlTextWriter xmltw = new XmlTextWriter(SignedFileName,
+                new UTF8Encoding(false)))
+            {
+                xmltw.WriteStartDocument();
+                doc.WriteTo(xmltw);
             }
         }
-        throw new Exception($"Certificate with thumbprint {thumbprint} not found");
-    }
-
-    public static void Main(string[] args)
-    {
-        string xmlFilePath = "./Certs/tosign.xml";
-        string certificateThumbprint = "";
-
-        SignXml(xmlFilePath, certificateThumbprint);
-
-        Console.ReadKey();
     }
 }
